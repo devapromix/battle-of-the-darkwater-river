fd966fbd00f705515f7f88d80cc3de1a19cac139
devapromix <maxwof@ukr.net>
2024-10-07 16:11:58 +0300

    Інтегрував модуль mouse

--- main.lua
+++ main.lua
@@ -15,13 +15,14 @@ function love.load(args)
 end
 
 function love.draw()
-    love.graphics.push()
-    local sx = love.graphics.getPixelWidth() / window.width
-    local sy = love.graphics.getPixelHeight() / window.height
-    love.graphics.scale(sx, sy)
-    local mx, my = love.mouse.getPosition()
-	states.draw(mx / sx, my / sy)
-    love.graphics.pop()
+	love.graphics.push()
+	local sx = love.graphics.getPixelWidth() / window.width
+	local sy = love.graphics.getPixelHeight() / window.height
+	love.graphics.scale(sx, sy)
+	local mx, my = love.mouse.getPosition()
+	mouse.set_pos(mx / sx, my / sy)
+	states.draw()
+	love.graphics.pop()
 end
 
 function love.update(dt)
